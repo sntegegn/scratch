882da932528496ddca04dd46bd1607da09c1cdc1
Sntegegn <selamnigussie41@gmail.com>
2023-10-16 01:16:42 -0400

    incr, pipeline

--- redis_go/main.go
+++ redis_go/main.go
@@ -17,6 +17,8 @@ func main() {
 		DB:       0,
 	})
 	ctx := context.Background()
+
+	//Set and Get
 	err := client.Set(ctx, "foo", "bar", 0).Err()
 	if err != nil {
 		log.Fatal("couldn't set value")
@@ -28,6 +30,31 @@ func main() {
 	}
 
 	fmt.Println(val)
+
+	//Increment
+
+	err = client.IncrBy(ctx, "counter", 5).Err()
+	if err != nil {
+		log.Fatal("couldn't increment a value")
+	}
+
+	ctr, _ := client.Get(ctx, "counter").Result()
+	fmt.Println("counter is: ", ctr)
+
+	//Pipeline
+
+	pipeline := client.Pipeline()
+
+	pipeline.Incr(ctx, "pipeline_val")
+	pipeline.Incr(ctx, "pipeline_val")
+	pipeline.Incr(ctx, "pipeline_val")
+
+	pipeline.Exec(ctx)
+
+	pipeline_val, _ := client.Get(ctx, "pipeline_val").Result()
+	fmt.Println("pipeline val is: ", pipeline_val)
+
+	//Publish
 
 	err = client.Publish(ctx, "mychannel", "how are you?").Err()
 	if err != nil {
