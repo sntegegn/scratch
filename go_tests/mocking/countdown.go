16a4015237df0b9ac9a7613f3f9eba2613923934
Sntegegn <selamnigussie41@gmail.com>
2023-10-30 10:10:30 -0700

    mocking

--- go_tests/mocking/countdown.go
+++ go_tests/mocking/countdown.go
@@ -10,14 +10,17 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
@@ -26,5 +29,10 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
+	/* for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	} */
+
 	fmt.Fprint(out, finalWord)
 }
